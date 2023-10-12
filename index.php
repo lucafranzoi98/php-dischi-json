73f31b5287a2bb16c1082d7aa0e4dc96d9869014
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-10-12 14:35:06 +0200

    add css and discs

--- index.php
+++ index.php
@@ -6,11 +6,26 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <title>Dischi</title>
 </head>
-<body>
+<body class="bg-dark text-white">
 
    <div id="app">
 
-      <div v-for="disc in discs">{{disc}}</div>
+      <h1 class="text-center mt-3">DISCS</h1>
+      <div class="container mt-3">
+         <div class="row">
+            <div v-for="disc in discs" class="col">
+               <div class="card h-100">
+                  <img :src="disc.poster" class="card-img-top">
+                  <div class="card-body text-center d-flex flex-column justify-content-between">
+                     <h6 class="card-title">{{disc.title}}</h6>
+                     <h6 class="card-text">{{disc.author}}</h6>
+                     <h6 class="card-text">{{disc.year}}</h6>
+                  </div>
+               </div>               
+            </div>
+         </div>
+      </div>
+      
 
    </div>
 
